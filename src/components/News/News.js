bd80a01d1f851134d03448e01ab3050858822365
Vishal Anand <vishal@signitysolutions.com>
2022-01-10 21:02:49 +0530

    Display the 100 news in feed

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import news from "../../news.json"
+import { Card, CardBody } from 'reactstrap';
 export default function News() {
+    console.log("news", news);
     return (
         <div>
-            News
+            {news?.map((item, index) => (
+                <Card key={item.id}>
+                    <CardBody>
+                        <h5>{item.title}</h5>
+                        <p>{item.body}</p>
+                    </CardBody>
+                </Card>
+            ))}
         </div>
     )
 }

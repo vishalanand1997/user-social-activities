a24b0f6eeb0a14e1e539f77b95e3fff43a71f0fe
Vishal Anand <vishal@signitysolutions.com>
2022-01-10 17:40:41 +0530

    Setuped the Routes and create the components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Login from "./components/Login/Login";
+import News from "./components/Posts/News";
+import SignUp from "./components/Signup/SignUp";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/news">
+          <News />
+        </Route>
+        <Route path="/signUp">
+          <SignUp />
+        </Route>
+        <Route path="/" exact>
+          <Login />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
-
-export default App;
